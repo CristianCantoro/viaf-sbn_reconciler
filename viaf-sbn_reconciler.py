93b1c91964cc622570b4005c02c601c502156885
CristianCantoro <kikkocristian@gmail.com>
2013-07-07 17:09:29 +0200

    Renaming ICCU to SBN

--- viaf-sbn_reconciler.py
+++ viaf-sbn_reconciler.py
@@ -3,8 +3,8 @@
 
 from csv import reader
 
-INFILESBN = '6600SBN.csv'
-INFILEVIAF = 'VIAF-ICCU.csv'
+INFILESBN = '../6600SBN.csv'
+INFILEVIAF = '../VIAF-ICCU.csv'
 
 infilesbn = open(INFILESBN, 'r')
 sbn_reader = reader(infilesbn)
@@ -23,13 +23,13 @@ viaf_reader = reader(infileviaf)
 viaf = dict()
 for line in viaf_reader:
     viaf_code = line[0]
-    iccu_code = line[1].strip('IT\\ICCU\\')
-    viaf[viaf_code] = iccu_code
+    sbn_code = line[1].strip('IT\\ICCU\\')
+    viaf[viaf_code] = sbn_code
 infileviaf.close()
 
-viaf_iccu_codes = set(viaf.values())
+viaf_sbn_codes = set(viaf.values())
 sbn_codes = set(sbn.keys())
 
-print len(viaf)
-print len(sbn)
-print len(viaf_iccu_codes.intersection(sbn_codes))
+print 'no. of VIAF-ICCU SBN records: ', len(viaf)
+print 'no. of ICCU SBN records with description: ', len(sbn)
+print 'no. of elements in the intersection:', len(viaf_sbn_codes.intersection(sbn_codes))
